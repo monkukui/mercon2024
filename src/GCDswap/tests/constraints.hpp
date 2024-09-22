346615d9a1959a9cbdd3f4d4968634d9d99961ee
h-a-kumakura <h-a-kumakura@mercari.com>
2024-09-22 18:14:45 +0900

    remove unused problem

--- src/GCDswap/tests/constraints.hpp
+++
@@ -1,4 +0,0 @@
-const long long int MIN_N = 2;
-const long long int MAX_N = 100000;
-const long long int MIN_A = 1;
-const long long int MAX_A = 9;
--- src/TwoEverywhere/tests/constraints.hpp
+++
@@ -1,2 +0,0 @@
-const long long int MIN_N = 2;
-const long long int MAX_N = 22;
