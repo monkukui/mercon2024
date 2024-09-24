56791a22a214d8a39706f72aa7a7a1815c8c9ef2
h-a-kumakura <h-a-kumakura@mercari.com>
2024-09-24 15:38:00 +0900

    change to 3%

--- src/Mercard/kuma_python_AC/main.py
+++ src/Mercard/kuma_python_AC/main.py
@@ -3,34 +3,28 @@
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
 
-# dp[1%で購入した商品の金額の合計][2%で購入した商品の金額の合計][3%で購入した商品の金額の合計] = 得たポイント
-dp = [[[-1 for k in range(X * 2)] for j in range(X * 2)] for i in range(X * 2)]
-dp[0][0][0] = 0
+# dp[1%で購入した商品の金額の合計][2%で購入した商品の金額の合計] = 得たポイント
+dp = [[-1 for k in range(X * 2)] for j in range(X * 2)]
+dp[0][0] = 0
 
 for i in range(N):
     a = A[i]
     for j in range(2 * X - 1, -1, -1):
         for k in range(2 * X - 1, -1, -1):
-            for l in range(2 * X - 1, -1, -1):
-                if dp[j][k][l] == -1:
-                    continue
-                if j < X:
-                    dp[j + a][k][l] = max(dp[j + a][k][l], dp[j][k][l] + a)
-                if k < X:
-                    dp[j][k + a][l] = max(dp[j][k + a][l], dp[j][k][l] + a * 2)
-                if l < X:
-                    dp[j][k][l + a] = max(dp[j][k][l + a], dp[j][k][l] + a * 3)
+            if dp[j][k] == -1:
+                continue
+            if j < X:
+                dp[j + a][k] = max(dp[j + a][k], dp[j][k] + a)
+            if k < X:
+                dp[j][k + a] = max(dp[j][k + a], dp[j][k] + a * 2)
 
 sum_a = sum(A)
 ans = 0
 for i in range(2 * X):
-    ans = max(ans, dp[i][0][0])
+    ans = max(ans, dp[i][0])
     if i >= X:
         for j in range(2 * X):
-            ans = max(ans, dp[i][j][0])
+            ans = max(ans, dp[i][j])
             if j >= X:
-                for k in range(2 * X):
-                    ans = max(ans, dp[i][j][k])
-                    if k >= X and dp[i][j][k] != -1:
-                        ans = max(ans, dp[i][j][k] + 4 * (sum_a - i - j - k))
+                ans = max(ans, dp[i][j] + 4 * (sum_a - i - j))
 print(ans)
