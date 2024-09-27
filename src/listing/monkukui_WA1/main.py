ed4f8763cf802ff253174ba0a9d843b1135063c6
monkukui <monkukui1953@gmail.com>
2024-09-27 20:32:57 +0900

    monkukui-check-A

---
+++ src/listing/monkukui_WA1/main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+n, p = map(int, input().split())
+max_p_sold_out = 0
+min_p_on_sale = 10**7
+
+for _ in range(n):
+    price, status = input().split()
+    price = int(price)
+    if status == "sold_out":
+        max_p_sold_out = max(max_p_sold_out, price)
+    else:
+        min_p_on_sale = min(min_p_on_sale, price)
+
+if max_p_sold_out < p <= min_p_on_sale:
+    print("YES")
+else:
+    print("NO")
