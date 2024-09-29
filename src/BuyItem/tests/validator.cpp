dfd2d37c0e163ad76600ce68ffbf2d0ccfab3d4b
h-a-kumakura <h-a-kumakura@mercari.com>
2024-09-30 05:05:01 +0900

    fix validator

--- src/BuyItem/tests/validator.cpp
+++ src/BuyItem/tests/validator.cpp
@@ -12,7 +12,7 @@ int main(){
     inf.readEoln();
 
     map<pair<int, int>, bool> uv_map;
-    map<int, bool> h_map;
+    map<pair<int, int>, bool> hv_map;
 
     for (int i = 0; i < N; i++) {
         int u = inf.readInt(MIN_U, MAX_U);
@@ -21,10 +21,14 @@ int main(){
         inf.readSpace();
         int h = inf.readInt(MIN_H, MAX_H);
         inf.readEoln();
-        assert((uv_map.find(pair<int, int>{u, v}) == uv_map.end(), "check input u, v"));
-        assert((h_map.find(h) == h_map.end(), "check input h"));
+        if (uv_map.find(pair<int, int>{u, v}) != uv_map.end()) {
+            quitf(_wa, "error: same input u, v");
+        }
+        if (hv_map.find(pair<int, int>{h, v}) != hv_map.end()) {
+            quitf(_wa, "error: same input h");
+        }
         uv_map[pair<int, int>{u, v}] = true;
-        h_map[h] = true;
+        hv_map[pair<int, int>{h, v}] = true;
     }
     inf.readEof();
     return 0;
