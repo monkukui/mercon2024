47d4ce234caef5af49187546caf009631a78a64b
h-a-kumakura <h-a-kumakura@mercari.com>
2024-10-03 14:50:03 +0900

    add assert

--- src/listing/kuma_AC/main.cpp
+++ src/listing/kuma_AC/main.cpp
@@ -2,17 +2,22 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <cassert>
 #define ll long long
 using namespace std;
 
 int main() {
     ll N, P;
     cin >> N >> P;
+    assert((2 <= N) && (N <= 100));
+    assert((300 <= P) && (P <= 9999999));
     bool ok = true;
     for (ll i = 0; i < N; ++i) {
         ll C;
         string S;
         cin >> C >> S;
+        assert((300 <= C) && (C <= 9999999));
+        assert((S == "on_sale") || (S == "sold_out"));
         if (S == "on_sale") {
             if (P >= C) {
                 ok = false;
